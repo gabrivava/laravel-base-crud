84351dcd48ed8128726f00b15397a2a4d88f1978
gabrivava <gabriele.vavassori4@gmail.com>
2021-07-14 14:43:45 +0200

    creato rotte CRUD

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* singola route home */
+Route::get('/', 'PageController@home')->name('home');
+
+/* 7 route CRUD */
+/* Route::resource('movies', 'MovieController'); */
+
+/* equivale ad aver scritto questo */
+Route::get('movies', 'MovieController@index')->name('movies');
+Route::get('movies/create', 'MovieController@create')->name('movies.create');
+Route::post('movies', 'MovieController@store')->name('movies.store');
+Route::get('movies/{movie}', 'MovieController@show')->name('movies.show');
+Route::get('movies/{movie}/edit', 'MovieController@edit')->name('movies.edit');
+Route::put('movies/{movie}', 'MovieController@update')->name('movies.update');
+Route::delete('movies/{movie}', 'MovieController@destoy')->name('movies.destroy');
